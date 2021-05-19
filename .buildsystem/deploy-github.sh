80114ecf0c3c219eb722b847fa669918d582fb59
Anton Malinskiy <anton@malinskiy.com>
2021-05-20 09:30:55 +1000

    ci(all): deploy new modules

--- .buildsystem/deploy-github.sh
+++ .buildsystem/deploy-github.sh
@@ -16,13 +16,15 @@ if [ -z "$GPG_PASSPHRASE" ]; then
   exit 1
 fi
 
-DTASK=":adam:publishDefaultPublicationToGitHubRepository"
-TARGETS="$DTASK"
+ATASK=":adam:assemble :adam-junit4-android:assemble :adam-junit4-android-contract:assemble"
+DTASK=":adam:publishDefaultPublicationToOSSHRRepository :adam-junit4-android:publishDefaultPublicationToOSSHRRepository :adam-junit4-android-contract:publishDefaultPublicationToOSSHRRepository"
 
 if [ -n "$GIT_TAG_NAME" ]; then
   echo "on a tag -> deploy release version $GIT_TAG_NAME"
-  ./gradlew "$TARGETS" -PreleaseMode=RELEASE
+  ./gradlew "$ATASK" -PreleaseMode=RELEASE
+  ./gradlew "$DTASK" -PreleaseMode=RELEASE
 else
   echo "not on a tag -> deploy snapshot version"
-  ./gradlew "$TARGETS" -PreleaseMode=SNAPSHOT
+  ./gradlew "$ATASK" -PreleaseMode=SNAPSHOT
+  ./gradlew "$DTASK" -PreleaseMode=SNAPSHOT
 fi
--- .buildsystem/deploy-sonatype.sh
+++ .buildsystem/deploy-sonatype.sh
@@ -16,15 +16,17 @@ if [ -z "$GPG_PASSPHRASE" ]; then
   exit 1
 fi
 
-DTASK=":adam:publishDefaultPublicationToOSSHRRepository"
-TARGETS="$DTASK"
+ATASK=":adam:assemble :adam-junit4-android:assemble :adam-junit4-android-contract:assemble"
+DTASK=":adam:publishDefaultPublicationToOSSHRRepository :adam-junit4-android:publishDefaultPublicationToOSSHRRepository :adam-junit4-android-contract:publishDefaultPublicationToOSSHRRepository"
 
 echo "Value of TEST_ENVVAR is $TEST_ENVVAR"
 
 if [ -z "$GIT_TAG_NAME" ]; then
   echo "not on a tag -> deploy snapshot version"
-  ./gradlew "$TARGETS" -PreleaseMode=SNAPSHOT --info --stacktrace
+  ./gradlew "$ATASK" -PreleaseMode=SNAPSHOT
+  ./gradlew "$DTASK" -PreleaseMode=SNAPSHOT
 else
   echo "on a tag -> deploy release version $GIT_TAG_NAME"
-  ./gradlew "$TARGETS" -PreleaseMode=RELEASE
+  ./gradlew "$ATASK" -PreleaseMode=RELEASE
+  ./gradlew "$DTASK" -PreleaseMode=RELEASE
 fi
